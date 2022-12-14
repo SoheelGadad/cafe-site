cde6aac819a5eb0856b60002fe1336c67afb4ce0
SoheelGadad <108049316+SoheelGadad@users.noreply.github.com>
2022-12-14 21:03:36 +0530

    UI Update

---
+++ frontend/UI/src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+import Main from "./components/main";
+import Book from "./components/book";
+import ThankYou from "./components/thankYou";
+import Navbar from "./components/navbar";
+import Login from "./components/login";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import main from "./components/main";
+
+export default (_) => {
+  const [page, setPage] = useState(0);
+
+  return (
+    //In is we have created page for site
+    <div>
+      <Navbar setPage={setPage} />
+      {page === 0 ? <Main setPage={setPage} /> : null}
+      {page === 1 ? <Book setPage={setPage} /> : null}
+      {page === 2 ? <ThankYou /> : null}
+      {page === 3 ? <Login setPage={setPage} /> : null}
+    </div>
+  );
+};
