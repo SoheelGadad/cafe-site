f8c8ce02e1be7f98b1fa1f47d9ade09533213101
SoheelGadad <108049316+SoheelGadad@users.noreply.github.com>
2022-12-20 18:07:40 +0530

    update

--- api/models/user.module.js
+++ api/models/user.module.js
@@ -5,6 +5,15 @@ const User = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    pic: {
+      type: String,
+      required: true,
+      default:
+        "https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg",
+    },
+  },
+  {
+    timestamps: true,
   },
   { collection: "user-data" }
 );
