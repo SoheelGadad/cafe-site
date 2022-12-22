ac4a794908cb7ba58569a2414e9ddb6ee0a85766
Soheel Gadad <108049316+SoheelGadad@users.noreply.github.com>
2022-12-22 16:41:40 +0530

    Update app.js

--- api/app.js
+++ api/app.js
@@ -61,7 +61,6 @@ app.post("/api/login", async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
-      token: generateToken(user._id),
     });
   } else {
     res.status(401);
