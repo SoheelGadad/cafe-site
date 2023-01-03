c6f8a7a924925c1697cc0f4cf221cbf432c0bd1f
Soheel Gadad <108049316+SoheelGadad@users.noreply.github.com>
2023-01-03 18:38:27 +0530

    update

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -3,7 +3,6 @@ import { NavbarBrand, NavLink } from "reactstrap";
 import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import "./navbarStyle.css";
-import { Container } from "react-bootstrap";
 
 import { logout } from "../actions/userActions";
 const Headers = (setSearch) => {
@@ -20,21 +19,25 @@ const Headers = (setSearch) => {
 
   const Rendermenu = () => {
     return (
-      <Nav className="me-auto">
-        <Nav.Link className="link" href="/">
-          Home
-        </Nav.Link>
+      <ul className="nav-menu">
+        <li className="nav-links">
+          <NavLink href="/" activeClassName="active" className="nav-links">
+            Home
+          </NavLink>
+        </li>
+
         {userInfo ? (
           <>
-            <NavLink href="/Book" className="link">
-              Book A Table
-            </NavLink>
+            <li>
+              <a href="/Book">Book A Table</a>
+            </li>
+
             <NavDropdown
               title={`${userInfo.name}`}
               id="collasible-nav-dropdown"
               className="link"
             >
-              <NavDropdown.Item href="/userprofile">
+              <NavDropdown.Item href="/userprofile" className="li">
                 <img
                   alt=""
                   src={`${userInfo.pic}`}
@@ -46,31 +49,32 @@ const Headers = (setSearch) => {
               </NavDropdown.Item>
 
               <NavDropdown.Divider />
-              <NavDropdown.Item onClick={logoutHandler}>
-                Logout
-              </NavDropdown.Item>
+              <li>
+                <a onClick={logoutHandler}>Logout</a>
+              </li>
             </NavDropdown>
           </>
         ) : (
-          <NavLink className="link" href="/Login">
-            Login
-          </NavLink>
+          <li>
+            <a href="/Login">Login</a>
+          </li>
         )}
-      </Nav>
+        <li>
+          <a href="#">about</a>
+        </li>
+      </ul>
     );
   };
   return (
-    <Navbar bg="dark" expand="sm">
-      <Container>
-        <NavbarBrand className="nav-brand">
-          <NavLink href="/">Cafe shop</NavLink>
-        </NavbarBrand>
-        <Navbar.Toggle className="toggle" aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Rendermenu />
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div className="nav">
+      <div className="nav-container">
+        <NavLink href="/" className="nav-logo">
+          Cafe shop
+        </NavLink>
+
+        <Rendermenu />
+      </div>
+    </div>
   );
 };
 
