774c4d91bbb3ab4dc283c644b6c1542d30c7144a
Soheel Gadad <108049316+SoheelGadad@users.noreply.github.com>
2023-01-06 11:53:47 +0530

    Update user.js

---
+++ api/Controller/user.js
@@ -0,0 +1,76 @@
+const User = require("../models/userModel");
+
+const nodemailer = require("nodemailer");
+
+const sendotp = async (req, res) => {
+  console.log(req.body);
+  const _otp = Math.floor(100000 + Math.random() * 900000);
+  console.log(_otp);
+  let user = await User.findOne({ email: req.body.email });
+  // send to user mail
+  if (!user) {
+    res.send({ code: 500, message: "user not found" });
+  }
+
+  //let User = await nodemailer.createTestAccount();
+
+  let transporter = nodemailer.createTransport({
+    service: `gmail`,
+    host: `smtp.gmail.com`,
+    port: 465,
+    secure: true,
+    auth: {
+      user: User.name,
+      pass: User.password,
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: "soheelgadad16@gmail.com",
+    to: req.body.email, // list of receivers
+    subject: "OTP", // Subject line
+    text: String(_otp),
+    html: `<html>
+              < body >
+              Hello and welcome
+          </ >
+         </html > `,
+  });
+
+  if (info.messageId) {
+    console.log(info, 84);
+    User.updateOne({ email: req.body.email }, { otp: _otp })
+      .then((result) => {
+        res.send({ code: 200, message: "otp send" });
+      })
+      .catch((err) => {
+        res.send({ code: 500, message: "Server err" });
+      });
+  } else {
+    res.send({ code: 500, message: "Server err" });
+  }
+};
+
+const submitotp = (req, res) => {
+  console.log(req.body);
+
+  User.findOne({ otp: req.body.otp })
+    .then((result) => {
+      //  update the password
+
+      User.updateOne({ email: result.email }, { password: req.body.password })
+        .then((result) => {
+          res.status(200);
+          throw new Error("Password updated");
+        })
+        .catch((err) => {
+          res.status(404);
+          throw new Error("Server err");
+        });
+    })
+    .catch((err) => {
+      res.status(500);
+      throw new Error("otp is wrong");
+    });
+};
+module.exports = { sendotp, submitotp };
